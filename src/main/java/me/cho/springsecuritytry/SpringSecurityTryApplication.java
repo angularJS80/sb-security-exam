f49afef84d40ffa4fa9b685f0c1eefd411614375
angularJS80 <bigcen@naver.com>
2019-01-05 23:26:28 +0900

    spring-security-bagic

---
+++ src/main/java/me/cho/springsecuritytry/SpringSecurityTryApplication.java
@@ -0,0 +1,14 @@
+package me.cho.springsecuritytry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityTryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityTryApplication.class, args);
+	}
+
+}
+
